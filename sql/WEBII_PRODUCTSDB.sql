e719547a529db3a0b2ec2595a5d619febceddcd4
Gabriel <gab.bmar.05@gmail.com>
2024-04-30 09:27:28 -0300

    30-4 commit se modifico la DB agregando la tabla usuarios

--- sql/WEBII_PRODUCTSDB.sql
+++ sql/WEBII_PRODUCTSDB.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-04-29 21:20:53.552
+-- Last modification date: 2024-04-30 12:18:51.254
 
 -- tables
 -- Table: categoria
@@ -9,21 +9,32 @@ CREATE TABLE categoria (
     CONSTRAINT categoria_pk PRIMARY KEY (idCat)
 );
 
--- Table: products
-CREATE TABLE products (
+-- Table: productos
+CREATE TABLE productos (
     idProducto int  NOT NULL AUTO_INCREMENT,
     nombre varchar(30)  NOT NULL,
-    descripcion varchar(100)  NOT NULL,
+    descripcion varchar(100)  NULL,
     precio decimal(10,2)  NOT NULL,
-    stock boolean  NOT NULL,
+    stock boolean  NOT NULL DEFAULT 1,
     idCategoria int  NOT NULL,
     categoria_idCat int  NOT NULL,
-    CONSTRAINT products_pk PRIMARY KEY (idProducto)
+    CONSTRAINT productos_pk PRIMARY KEY (idProducto)
+);
+
+-- Table: usuarios
+CREATE TABLE usuarios (
+    idUsuario int  NOT NULL AUTO_INCREMENT,
+    email varchar(20)  NOT NULL,
+    password varchar(20)  NOT NULL,
+    nombre varchar(20)  NULL,
+    apellido varchar(20)  NULL,
+    admin boolean  NOT NULL,
+    CONSTRAINT usuarios_pk PRIMARY KEY (idUsuario)
 );
 
 -- foreign keys
--- Reference: products_categoria (table: products)
-ALTER TABLE products ADD CONSTRAINT products_categoria FOREIGN KEY products_categoria (categoria_idCat)
+-- Reference: products_categoria (table: productos)
+ALTER TABLE productos ADD CONSTRAINT products_categoria FOREIGN KEY products_categoria (categoria_idCat)
     REFERENCES categoria (idCat);
 
 -- End of file.
