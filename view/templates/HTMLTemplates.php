eab0aefbfc16d718658a45b26ddf96ece79e04f7
Lucas Irigoyen <lucasairigoyen@gmail.com>
2024-05-30 19:42:31 -0300

    modifique mensaje de error

--- view/templates/HTMLTemplates.php
+++ view/templates/HTMLTemplates.php
@@ -264,7 +264,7 @@ function HTMLprofile($User = null){
 function HTMLerrorPage(){
     ?>
     <section class="error__container">
-        <h1>La carda de datos no ha sido correcta.</h1>
+        <h1>La carga de datos no ha sido correcta.</h1>
     </section>
     <?php
 }
