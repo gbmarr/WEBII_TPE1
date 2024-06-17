c416f80718cab51e2cd0f15c4fa0448801b53506
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-16 23:10:16 -0300

    Modificacion del archivo config. Solo persisten constantes de DB

---
+++ config/ConfigApp.php
@@ -0,0 +1,7 @@
+<?php
+
+// constantes pertenecientes a la conexion con DB
+const SQL_USER = "root";
+const SQL_PASS = "";
+const SQL_DBNAME = "webii_productsdb;charset=utf8";
+const SQL_HOST = "localhost";
