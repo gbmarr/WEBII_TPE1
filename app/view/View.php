1e307f1463c3249b7aee625b392153e80a3f0b67
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-19 17:36:10 -0300

    Agrego clase View para manipular los templates

---
+++ app/view/View.php
@@ -0,0 +1,12 @@
+<?php
+require_once './libs/Smarty.class.php';
+require_once './BASE_URL.php';
+
+class View{
+    protected $smarty;
+
+    function __construct(){
+        $this->smarty = new Smarty();
+        $this->smarty->assign('base', BASE_URL);
+    }
+}
