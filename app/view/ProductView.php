88924b772f5a724022aacc26e584fcec3999de3f
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-22 16:25:54 -0300

    Agrego logica inicial a las vistas en ProductView + modifico constructor en archivo View

--- app/view/ProductView.php
+++ app/view/ProductView.php
@@ -4,12 +4,18 @@ require_once 'View.php';
 class ProductView extends View{
 
     function showAllProducts($products){
-        
+        $this->smarty->assign('products', $products);
+        $this->smarty->display('products/productCards.tpl');
     }
 
     function showProductDetail($product){
+        $this->smarty->assign('product', $product);
+        $this->smarty->display('products/productDetail.tpl');
     }
 
-    function showProductForm($product = null){
+    function showProductForm($product = null, $categories = []){
+        $this->smarty->assign('product', $product ? $product : null);
+        $this->smarty->assign('categories', $categories);
+        $this->smarty->display('products/productForm.tpl');
     }
 }
--- app/view/View.php
+++ app/view/View.php
@@ -7,6 +7,7 @@ class View{
 
     function __construct(){
         $this->smarty = new Smarty();
-        $this->smarty->assign('base', BASE_URL);
+        // $this->smarty->assign('base', BASE_URL);
+        $this->smarty->setTemplateDir('templates/');
     }
 }
