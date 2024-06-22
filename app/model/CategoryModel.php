727a724abac83e5226e712364e202e92df3ebe42
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-22 16:13:10 -0300

    Agrego logica de modelo en CategoryModel

--- app/model/CategoryModel.php
+++ app/model/CategoryModel.php
@@ -1,25 +1,33 @@
 <?php
+require_once 'Model.php';
 
-class CategoryModel{
+class CategoryModel extends Model {
 
 
     function getAllCategories(){
-
+        $query = "SELECT `idCat`, `descripcionCat` FROM `categoria`";
+        $categories = $this->executeQuery($query);
+        return $categories->fetchAll(PDO::FETCH_OBJ);
     }
 
     function getCategoryByID($id){
-
+        $query = "SELECT `idCat`, `descripcionCat` FROM `categoria` WHERE `idCat` = ?";
+        $category = $this->executeQuery($query, [$id]);
+        return $category->fetch(PDO::FETCH_OBJ);
     }
 
-    function addCategory($name){
-
+    function addCategory($newCategory){
+        $query = "INSERT INTO `categoria` (`descripcionCat`) VALUES (?)";
+        $this->executeQuery($query, [$newCategory]);
     }
 
-    function updateCategory($id, $name){
-
+    function updateCategory($id, $newName){
+        $query = "UPDATE `categoria` SET `descripcionCat` = ? WHERE `idCat` = ?";
+        $this->executeQuery($query, [$newName, $id]);
     }
 
     function deleteCategory($id){
-        
+        $query = "DELETE FROM `categoria` WHERE `idCat` = ?";
+        $this->executeQuery($query, [$id]);
     }
 }
