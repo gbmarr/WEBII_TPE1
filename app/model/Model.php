c92ebaa27a11557b5613ca0c824447b835871629
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-24 17:06:44 -0300

    Modifico estructura y metodos en archivo Model

--- app/model/Model.php
+++ app/model/Model.php
@@ -3,34 +3,42 @@ require_once './config/ConfigApp.php';
 
 class Model{
     protected $database;
-    protected $conn;
 
     function __construct(){
         try {
-            $this->conn = "mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";charset=utf8";
-            $this->database = new PDO($this->conn, SQL_USER, SQL_PASS);
+            $this->deploy();
+            $data = "mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";charset=utf8";
+            $this->database = new PDO($data, SQL_USER, SQL_PASS);
             $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    // private function createOrUseDatabase($database){
-    //     $this->database->exec("CREATE DATABASE IF NOT EXISTS $database");
-    //     $this->database->exec("USE $database");
-    // }
+    private function deploy(){
+        $sql = file_get_contents('data/database.sql');
+        try {
+            $data = "mysql:host=".SQL_HOST."";
+            $this->database = new PDO($data, SQL_USER, SQL_PASS);
+            $this->database->exec($sql);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 
-    // private function createTable(){
+    private function useDatabase(){
+        try {
+            $dbExist = $this->database->query("SHOW DATABASES LIKE '".SQL_DBNAME."'");
+            if(!($dbExist->rowCount() > 0)){
+                $this->database->exec("CREATE DATABASE ".SQL_DBNAME."");
+            }
+            $this->database->query("USE ".SQL_DBNAME."");
 
-    // }
-
-    // private function deploy(){
-    //     $this->createOrUseDatabase('');
-    //     $this->createTable();
-    // }
-
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
     function executeQuery($query, $params = []){
         try {
             $action = $this->database->prepare($query);
