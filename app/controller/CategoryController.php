03efa6b23e7ad6bc40bb9da7e2efc5057a12c26f
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-24 12:01:10 -0300

    Agrego logica inicial a CategoryController

--- app/controller/CategoryController.php
+++ app/controller/CategoryController.php
@@ -1,26 +1,45 @@
 <?php
+require_once 'app/model/CategoryModel.php';
+require_once 'app/view/CategoryView.php';
 
 class CategoryController{
-    
+    private $categoryModel;
+    private $categoryView;
+
+    function __construct(){
+        $this->categoryModel = new CategoryModel();
+        $this->categoryView = new CategoryView();
+    }
+
+    function viewAllCategories(){
+        $categories = $this->categoryModel->getAllCategories();
+        $this->categoryView->showAllCategories($categories);
+    }
 
     function createCategory(){
-
+        $this->categoryView->showCategoryForm();
     }
 
     function storeCategory(){
-
+        $categoryName = $_POST['category_name'];
+        $this->categoryModel->addCategory($categoryName);
+        header('Location: ' . BASE_URL . '');
     }
 
     function editCategory($id){
-
+        $category = $this->categoryModel->getCategoryByID($id);
+        $this->categoryView->showCategoryForm($category);
     }
 
     function updateCategory($id){
-        
+        $categoryName = $_POST['category_name'];
+        $this->categoryModel->updateCategory($id, $categoryName);
+        header('Location: ' . BASE_URL . '');
     }
 
     function deleteCategory($id){
-
+        $this->categoryModel->deleteCategory($id);
+        header('Location: ' . BASE_URL . '');
     }
 
     function errorCategory(){
