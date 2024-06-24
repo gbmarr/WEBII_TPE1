492cbf8d7be021d02f24cc9adf58abc721b75ad4
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-24 12:29:23 -0300

    Agrego metodo deploy, createTable y createOrUse... en archivo Model

--- app/model/Model.php
+++ app/model/Model.php
@@ -2,20 +2,35 @@
 require_once './config/config.php';
 
 class Model{
-    private $database;
-    private $conn;
+    protected $database;
+    protected $conn;
 
     function __construct(){
         try {
-
             $this->conn = "mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME.";charset=utf8";
             $this->database = new PDO($this->conn, SQL_USER, SQL_PASS);
             $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            
 
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
+
+    // private function createOrUseDatabase($database){
+    //     $this->database->exec("CREATE DATABASE IF NOT EXISTS $database");
+    //     $this->database->exec("USE $database");
+    // }
+
+    // private function createTable(){
+
+    // }
+
+    // private function deploy(){
+    //     $this->createOrUseDatabase('');
+    //     $this->createTable();
+    // }
 
     function executeQuery($query, $params = []){
         try {
