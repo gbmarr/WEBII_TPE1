e3941026096b8e84785ebfc712209ccde1de6840
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-25 19:40:15 -0300

    Agrego restricción de vistas en controlador de categorías

--- app/controller/CategoryController.php
+++ app/controller/CategoryController.php
@@ -1,8 +1,9 @@
 <?php
 require_once 'app/model/CategoryModel.php';
 require_once 'app/view/CategoryView.php';
+require_once 'app/controller/AuthController.php';
 
-class CategoryController{
+class CategoryController extends AuthController{
     private $categoryModel;
     private $categoryView;
 
@@ -12,37 +13,43 @@ class CategoryController{
     }
 
     function viewAllCategories(){
+        $this->checkAdmin();
         $categories = $this->categoryModel->getAllCategories();
         $this->categoryView->showAllCategories($categories);
     }
 
     function createCategory(){
+        $this->checkAdmin();
         $this->categoryView->showCategoryForm();
     }
 
     function storeCategory(){
+        $this->checkAdmin();
         $categoryName = $_POST['name'];
         $this->categoryModel->addCategory($categoryName);
         header('Location: ' . BASE_URL . '');
     }
 
     function editCategory($id){
+        $this->checkAdmin();
         $category = $this->categoryModel->getCategoryByID($id);
         $this->categoryView->showCategoryForm($category);
     }
 
     function updateCategory($id){
+        $this->checkAdmin();
         $categoryName = $_POST['name'];
         $this->categoryModel->updateCategory($id, $categoryName);
         header('Location: ' . BASE_URL . '');
     }
 
     function deleteCategory($id){
+        $this->checkAdmin();
         $this->categoryModel->deleteCategory($id);
         header('Location: ' . BASE_URL . '');
     }
 
     function errorCategory(){
-        
+        $this->categoryView->showErrorCategory();
     }
 }
