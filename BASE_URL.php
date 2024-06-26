4d797166e88f7001d691c5f40e647d5db253fae3
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-06-26 13:21:16 -0300

    Agrego inicio de sesión en archivo BASE_URL

--- BASE_URL.php
+++ BASE_URL.php
@@ -1,4 +1,4 @@
 <?php
 // definicion de la URLBASE para trabajar con ella desde 'acciones'
-
+session_start();
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']));
